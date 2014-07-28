3cd7ba82e7a99d79b860e9554b678c8e5b1b49ac
alejandrocan <darknesstp@gmail.com>
2014-07-27 20:49:48 -0500

    Base De datos
    Classe principal

--- Facturacion/src/facturador/ConexDBase.java
+++ Facturacion/src/facturador/ConexDBase.java
@@ -1,6 +1,8 @@
 package facturador;
 
 import java.sql.*;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class ConexDBase {
     public static final String USER = "root";
@@ -52,7 +54,7 @@ public class ConexDBase {
         try {
             Statement comand = conect.createStatement();
             comand.executeUpdate("insert into factura values('"+folio+"','"+rfc+"','"+nombre+"','"+direccion+"','"+email+"','"+cod_postal+"','"+nacionalidad+"','"+servicio+"','"+cantidad+"','"+costo+"','"+subtotal+"','"+total+"','"+comentario+"','"+estado_factura+"',now())");
-            conect.close();
+            //conect.close();
             return true;
         } catch (Exception e) {
             System.out.println(e);
@@ -100,5 +102,69 @@ public class ConexDBase {
             System.out.println(e);
         }
         return folio;
+    }
+    public void ShowUsers(JTable tabla){
+            DefaultTableModel modelo = (DefaultTableModel) tabla.getModel();
+        try{
+            
+            Statement coman = conect.createStatement();
+            ResultSet row = coman.executeQuery("SELECT * FROM usuario");
+            Object[] fila = new Object[6];
+            while(row.next()){
+                fila[0] = row.getObject("nombre");
+                fila[1] = row.getObject("apellido");
+                fila[2] = row.getObject("id");
+                fila[3] = row.getObject("clave");
+                fila[4] = row.getObject("tipo_de_usuario");
+                fila[5] = row.getObject("nro");
+                modelo.addRow(fila);
+            }
+            tabla.setModel(modelo);
+        }catch(SQLException e){
+            
+        }
+    }
+    public int addUser(String nombre, String apellido, String clave, String id_user){
+        int numero = 0;
+        try{
+            Statement st = conect.createStatement();
+            Statement cm = conect.createStatement();
+            ResultSet row = cm.executeQuery("Select * from usuario order by nro");
+            row.last();
+            numero = (Integer)row.getObject("nro");
+            numero++;
+            st.executeUpdate("Insert into usuario values ('"+nombre+"','"+apellido+"','"+id_user+"','"+clave+"','No admin',"+numero+")");
+//            conect.close();
+        }catch(SQLException e){
+            System.out.println(e);
+        }
+        return numero;
+    }
+    public boolean delUser(String nombre, String apel, String tipo){
+        
+        System.out.println(tipo);
+        if(tipo.equals("Admin")){
+            return false;
+        } else {
+            try{
+                Statement cmd = conect.createStatement();
+                cmd.executeUpdate("DELETE FROM usuario where nombre='"+nombre+"' AND apellido ='"+apel+"'");
+                
+                return true;
+            }catch(SQLException e){
+                System.out.println(e);
+            }
+        }
+        return false;
+    }
+    public boolean updateUser(String nombre,String apel,String id,String clave, int nro){
+        try{
+            Statement cmd = conect.createStatement();
+            cmd.executeUpdate("UPDATE usuario SET nombre='"+nombre+"',apellido = '"+apel+"', id ='"+id+"', clave ='"+clave+"' where nro="+nro+"");
+            
+        }catch(SQLException e){
+            System.out.println(e);
+        }
+        return false;
     }
 }
